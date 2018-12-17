aa43d2665a223bfd8895a4923b90b1ee57306a36
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2018-12-18 00:06:59 +0530

    disabled torrebt parsing for now

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict'
 const fs = require('fs');
-const torrent = fs.readFileSync('./puppy.torrent');
-console.log(torrent.toString('utf8'));
+//const parseTorrent = require('parse-torrent-file');
+//const torrent = parseTorrent(fs.readFileSync('./puppy.torrent'));
+console.log(torrent);
